8acc8988de626c4b1203dd01e2135c3e51fe1e18
Parker J. Rule <parker.rule@tufts.edu>
2020-10-26 15:52:39 -0400

    Add jsxgraph-react-js (with demo)

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,32 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import JXGBoard from 'jsxgraph-react-js';
+
+// copied from example
+const logicJS = (brd: any) => {
+  brd.suspendUpdate();
+  var a = brd.create('slider', [[2, 8], [6, 8], [0, 3, 6]], { name: 'a' });
+  var b = brd.create('slider', [[2, 7], [6, 7], [0, 2, 6]], { name: 'b' });
+  var A = brd.create('slider', [[2, 6], [6, 6], [0, 3, 6]], { name: 'A' });
+  var B = brd.create('slider', [[2, 5], [6, 5], [0, 3, 6]], { name: 'B' });
+  var delta = brd.create('slider', [[2, 4], [6, 4], [0, 0, Math.PI]], { name: '&delta;' });
+
+  var c = brd.create('curve', [
+    function (t: number) { return A.Value() * Math.sin(a.Value() * t + delta.Value()); },
+    function (t: number) { return B.Value() * Math.sin(b.Value() * t); },
+    0, 2 * Math.PI], { strokeColor: '#aa2233', strokeWidth: 3 });
+  brd.unsuspendUpdate();
+}
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <JXGBoard
+      logic={logicJS}
+      boardAttributes={{ axis: true, boundingbox: [-12, 10, 12, -10] }}
+      style={{ border: "3px solid red" }}
+    />
   );
 }
 
