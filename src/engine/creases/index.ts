e6e9a23924bf2d50ac519b8b4288322352485be4
Parker J. Rule <parker.rule@tufts.edu>
2020-11-14 12:44:04 -0500

    Add tweaks/stubs from Thursday meeting

--- src/engine/creases/index.ts
+++ src/engine/creases/index.ts
@@ -1,2 +1,12 @@
 // TODO: univeral molecule.
+import { PackingGraph, PackingNode, PackingEdge, CreasesGraph, CreasesNode, CreasesEdge }  from "../packing";
+
+function cleanPacking(graph: PackingGraph): CreasesGraph {
+    // TODO
+}
+
+function generateMolecules(graph: CreasesGraph): any {
+    // TODO: We need a type for the final creases output.
+}
+
 export {};
--- src/engine/packing/index.ts
+++ src/engine/packing/index.ts
@@ -1,124 +1,140 @@
-import { matrix, math } from 'mathjs';
+import { matrix, math } from "mathjs";
 
 class Graph {
-    nodes: Set<Node>;
-    edges: Set<Edge>;
-    neighbors: Record<number, Set<Edge>>;
+  nodes: Set<Node>;
+  edges: Set<Edge>;
+  neighbors: Record<number, Edge[]>;
 
-    constructor() {
-        this.nodes = new Set();
-        this.edges = new Set();
-        this.neighbors = {};
-    }
+  constructor() {
+    this.nodes = new Set();
+    this.edges = new Set();
+    this.neighbors = {};
+  }
 
-    addNode(n: Node) {
-        if (this.neighbors[n.id] === undefined) {
-            this.neighbors[n.id] = new Set();
-            this.nodes.add(n);
-        } else {
-            throw new Error(`Node with ID ${n.id} already exists.`);
-        }
+  addNode(n: Node) {
+    if (this.neighbors[n.id] === undefined) {
+      this.neighbors[n.id] = [];
+      this.nodes.add(n);
+    } else {
+      throw new Error(`Node with ID ${n.id} already exists.`);
     }
+  }
 
-    addEdge(e: Edge) {
-        if (this.neighbors[e.from.id] === undefined) {
-            throw new Error(`Cannot add an edge from nonexistent node ${e.from.id}.`)
-        }
-        if (this.neighbors[e.to.id] === undefined) {
-            throw new Error(`Cannot add an edge to nonexistent node ${e.to.id}.`)
-        }
-        for (let neighbor of this.neighbors[e.from.id]) {
-            if (neighbor.to.id === e.to.id) {
-                throw new Error(`Edge ${e.from.id} -> ${e.to.id} already exists.`);
-            }
-        }
-        for (let neighbor of this.neighbors[e.to.id]) {
-            if (neighbor.to.id === e.from.id) {
-                throw new Error(`Edge ${e.from.id} -> ${e.to.id} already exists.`);
-            }
-        }
-        this.edges.add(e);
-        this.neighbors[e.to.id].add(e);
-        this.neighbors[e.from.id].add(e);
+  addEdge(e: Edge) {
+    if (this.neighbors[e.from.id] === undefined) {
+      throw new Error(`Cannot add an edge from nonexistent node ${e.from.id}.`);
     }
+    if (this.neighbors[e.to.id] === undefined) {
+      throw new Error(`Cannot add an edge to nonexistent node ${e.to.id}.`);
+    }
+    for (let neighbor of this.neighbors[e.from.id]) {
+      if (neighbor.to.id === e.to.id) {
+        throw new Error(`Edge ${e.from.id} -> ${e.to.id} already exists.`);
+      }
+    }
+    for (let neighbor of this.neighbors[e.to.id]) {
+      if (neighbor.to.id === e.from.id) {
+        throw new Error(`Edge ${e.from.id} -> ${e.to.id} already exists.`);
+      }
+    }
+    this.edges.add(e);
+    this.neighbors[e.to.id].push(e);
+    this.neighbors[e.from.id].push(e);
+  }
 }
 
-class PackingGraph extends Graph {};
+class PackingGraph extends Graph {
+  scaleFactor: number;
+
+  constructor() {
+    super();
+    this.scaleFactor = 0;
+  }
+
+  rescale() {
+    // TODO: compute optimal scale factor.
+  }
+}
 
 class TreeGraph extends Graph {
-    shortestPath(from: TreeNode, to: TreeNode) {
+  shortestPath(from: TreeNode, to: TreeNode) {}
 
-    }
-
-    /*
-    pack(): PackingGraph {
-        // TODO:
-        // Build shortest-path distance matrix l_{ij}.
-    }
-    */
+  pack(): PackingGraph {
+    // TODO:
+    // Build shortest-path distance matrix l_{ij}.
+  }
 }
 
 interface Node {
-    id: number;
-    [key: string]: any;
+  id: string;
+  [key: string]: any;
 }
 
 interface Edge {
-    to: Node;
-    from: Node;
-    [key: string]: any;
+  to: Node;
+  from: Node;
+  [key: string]: any;
 }
 
 class TreeNode implements Node {
-    readonly id: number;
+  readonly id: string;
 
-    constructor(id: number) {
-        this.id = id;
-    }
+  constructor(id: string) {
+    this.id = id;
+  }
 }
 
 class TreeEdge implements Edge {
-    readonly to: TreeNode;
-    readonly from: TreeNode;
-    readonly length: number;
+  readonly to: TreeNode;
+  readonly from: TreeNode;
+  readonly length: number;
 
-    constructor(to: TreeNode, from: TreeNode, length: number)  {
-        this.to = to;
-        this.from = from;
-        this.length = length;
-    }
+  constructor(to: TreeNode, from: TreeNode, length: number) {
+    this.to = to;
+    this.from = from;
+    this.length = length;
+  }
 }
 
 class PackingEdge implements Edge {
-    readonly to: PackingNode;
-    readonly from: PackingNode;
-    constructor(to: PackingNode, from: PackingNode) {
-        this.to = to;
-        this.from = from;
-    }
+  readonly to: PackingNode;
+  readonly from: PackingNode;
+  constructor(to: PackingNode, from: PackingNode) {
+    this.to = to;
+    this.from = from;
+  }
 }
 
 class PackingNode implements Node {
-    readonly id: number;
-    readonly x: number;
-    readonly y: number;
+  readonly id: string;
+  readonly x: number;
+  readonly y: number;
 
-    constructor(id: number, x: number, y: number) {
-        this.id = id;
-        this.x = x;
-        this.y = y;
-    }
+  constructor(id: string, x: number, y: number) {
+    this.id = id;
+    this.x = x;
+    this.y = y;
+  }
 }
+
+class CreasesEdge extends PackingEdge {}
+class CreasesNode extends PackingNode {}
+class CreasesGraph extends PackingGraph {}
 
 export function testOpt() {
-    // TODO: hardcode augmented Lagrangian and derivative, using clipping for inequality constraints.
-    const augLag = function(x: matrix, lambda: matrix, mu: number) {
-        const n = (math.size(x) - 1) / 2;
-        const obj = -x[math.size(x) - 1];
-
-    }
+  // TODO: hardcode augmented Lagrangian and derivative, using clipping for inequality constraints.
+  const augLag = function(x: matrix, lambda: matrix, mu: number) {
+    const n = (math.size(x) - 1) / 2;
+    const obj = -x[math.size(x) - 1];
+  };
 }
 
-
 // TODO: optimization stuff.
-export {};
+export {
+  CreasesGraph,
+  CreasesNode,
+  CreasesEdge,
+  PackingGraph,
+  PackingNode,
+  PackingEdge,
+};
